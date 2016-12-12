3e9504128fa9369464563da9d958ceb5e1707b68
Huangshuai <woshihuangshuai@hotmail.com>
2016-12-12 15:02:15 +0800

    update python script；关于reverb：如果句子结尾没有标点会严重影响提取的结果

--- src/extract-news-title-script-0.3.py
+++ src/extract-news-title-script-0.3.py
@@ -11,6 +11,8 @@ import os
 import glob
 from tqdm import tqdm
 
+end_punctuation = '.?!'
+
 cur_dir = '../data'
 reuters_news_title_folder = cur_dir + '/Reuters_news_title'
 bloomberg_news_title_folder = cur_dir + '/Bloomberg_news_title'
@@ -28,7 +30,7 @@ date_folder_list = glob.glob('../data/ReutersNews106521/*')
 pbar = tqdm(total=len(date_folder_list))
 for date_folder in date_folder_list:
     news_date = date_folder.split('/')[-1]
-    pbar.set_description('Extracting %s' % date_folder)
+    pbar.set_description('Extracting reuters news...')
     for news_file_dir in glob.glob(date_folder + '/*'):
         news_title_file_path = reuters_news_title_folder + '/' + news_date + '-%s' % file_num
         news_title_file = open(news_title_file_path, 'w')
@@ -37,6 +39,11 @@ for date_folder in date_folder_list:
         if str == '-- \n':
             str = news_file.readline()
         news_title = str.split('-- ')[-1]
+        news_title = news_title.strip('\n')
+        if  len(news_title) == 0:
+            continue
+        if news_title[-1] not in end_punctuation:
+            news_title += '.'
         news_title_file.write(news_title)
         news_file.close()
         news_title_file.close()
@@ -50,7 +57,7 @@ date_folder_list = glob.glob('../data/20061020_20131126_bloomberg_news/*')
 pbar = tqdm(total=len(date_folder_list))
 for date_folder in date_folder_list:
     news_date = ''.join(date_folder.split('/')[-1].split('-'))
-    pbar.set_description('Extracting %s' % date_folder)
+    pbar.set_description('Extracting bloomberg news...')
     for news_file_dir in glob.glob(date_folder + '/*'):
         news_title_file_path = bloomberg_news_title_folder + '/' + news_date + '-%s' % file_num
         news_title_file = open(news_title_file_path, 'w')
@@ -59,6 +66,11 @@ for date_folder in date_folder_list:
         if str == '-- \n':
             str = news_file.readline()
         news_title = str.split('-- ')[-1]
+        news_title = news_title.strip('\n')
+        if  len(news_title) == 0:
+            continue
+        if news_title[-1] not in end_punctuation:
+            news_title += '.'
         news_title_file.write(news_title)
         news_file.close()
         news_title_file.close()
