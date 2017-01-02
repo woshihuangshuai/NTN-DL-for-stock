d8c7a7f377cdaa73847a84447645abfbda46b440
huangshuai <332987750@qq.com>
2017-01-02 15:48:17 +0800

    add code

--- src/extract-event.py
+++ src/extract-event.py
@@ -4,75 +4,77 @@
 import re
 
 
-data_resource = ['reuters', 'bloomberg']
-reverb_dict = {}
-zpar_dict = {}
-event_list = []
-
+data_resource = ['bloomberg', 'reuters']
+total = 0
 
 for resource in data_resource:
+    reverb_dict = {} # {key, set}
+    zpar_dict = {}
+
     print('Extracting %s...' % resource)
+    # extract (arg1, rel, arg2)
     with open('../data/%s_reverb_result_v1.txt' % resource, 'r') as reverb_result_file:
         c = 0
         line = reverb_result_file.readline()
         while line:
             items = line.split('\t')
-            O_1 = re.sub(r'[0-9]{8}', '', items[2]).strip(' ')
-            P = items[3]
-            O_2 = items[4]
-            datetime = items[12].split(' ')[0]
+            arg1        = re.sub(r'[^a-z]+', ' ', items[2].lower()).strip()
+            relation    = re.sub(r'[^a-z]+', ' ', items[3].lower()).strip()
+            arg2        = re.sub(r'[^a-z]+', ' ', items[4].lower()).strip()
+            datetime    = items[12].split()[0]
             if re.match(r'[0-9]{8}', datetime) != None:
                 if datetime not in reverb_dict.keys():
-                    reverb_dict[datetime] = [(O_1, P, O_2)]
-                else:
-                    reverb_dict[datetime].append((O_1, P, O_2))
+                    reverb_dict[datetime] = [(arg1, relation, arg2)]
+                elif (arg1, relation, arg2) not in reverb_dict[datetime]:
+                    reverb_dict[datetime].append((arg1, relation, arg2))
                 c += 1
             line = reverb_result_file.readline()
         # print(len(reverb_dict.keys()))
-        print('total of (O_1, P, O_2): %d.' % c)
-
+        print('total of (arg1, relation, arg2): %d.' % c)
+    
+    # extract (sub, predicate, obj)
     with open('../data/%s_zpar_dep_result.txt' % resource, 'r') as zpar_result_file:
         c = 0
-        item = []
+        items = []
         line = zpar_result_file.readline()
         while line:
             if line != '\n':
-                item.append(line.strip('\n'))
+                items.append(line.strip('\n'))
                 line = zpar_result_file.readline()
                 continue
             else:
                 datetime = ''
-                sub = ''
-                predicate = ''
-                obj = ''
+                sub = set()
+                predicate =set()
+                obj = set()
 
-                t = item[0].split('\t')
+                t = items[0].split('\t')
                 datetime = t[0]
                 del t[0]
                 if t[-1] == 'SUB':
-                    sub = t[0]
-                for i in range(1,len(item)):
-                    t = item[i].split('\t')
+                    sub.add(re.sub(r'[^a-z]+', ' ', t[0]).strip())
+                for i in range(1,len(items)):
+                    t = items[i].split('\t')
                     if t[-1] == 'SUB':
-                        sub = t[0]
+                        sub.add(re.sub(r'[^a-z]+', ' ', t[0]).strip())
                     elif t[-1] == 'ROOT':
-                        predicate = t[0]
+                        predicate.add(re.sub(r'[^a-z]+', ' ', t[0]).strip())
                     elif t[-1] == 'OBJ':
-                        obj = t[0]
-
-                if re.match(r'[0-9]{8}', datetime) != None:
-                    if datetime != '' and sub != '' and predicate != '' and obj != '':
+                        obj.add(re.sub(r'[^a-z]+', ' ', t[0]).strip())
+                
+                if datetime != '' and len(sub) != 0 and len(predicate) != 0 and len(obj) != 0:            
+                    if re.match(r'[0-9]{8}', datetime) != None:
                         if datetime not in zpar_dict.keys():
                             zpar_dict[datetime] = [(sub, predicate, obj)]
-                        else:
+                        elif (sub, predicate, obj) not in zpar_dict[datetime]:
                             zpar_dict[datetime].append((sub, predicate, obj))
                         c += 1
-                item = []
+                items = []
                 line = zpar_result_file.readline()
         # print(len(reverb_dict.keys()))
         print('total of (sub, predicate, obj): %d.' % c)
 
-
+    # record to file
     f = open('../data/%s_reverb_extract_result.txt' % resource, 'w')
     for key in reverb_dict.keys():
         datetime = key
@@ -91,27 +93,38 @@ for resource in data_resource:
             f.write(s)
     f.close()
 
-    c = 0
+    # extract event
+    event_list = set()
     for key in reverb_dict.keys():
         reverb_list = reverb_dict[key]
         try:
             zpar_list = zpar_dict[key]
         except Exception as e:
-            print 'KEY ERROR: %s.' % e
+            # print 'KEY ERROR: %s.' % e
             continue
         for i in reverb_list:
-            O_1 = ''.join(i[0].split(' '))
-            P = ''.join(i[1].split(' '))
-            O_2 = ''.join(i[2].split(' '))
+            is_in = 0
             for j in zpar_list:
-                if j[0] in O_1 and j[1] in P and j[2] in O_2:
-                    event_list.append((key, i[0], i[1], i[2]))
-                    c += 1
+                for sub in j[0]:
+                    if sub in i[0] and sub != '':
+                        is_in += 1
+                        break
+                for predicate in j[1]:
+                    if predicate in i[1] and sub != '':
+                        is_in += 1
+                        break
+                for obj in j[2]:
+                    if obj in i[2] and sub != '':
+                        is_in += 1
+                        break
+                if is_in == 3 and i[0] != '' and i[1] != '' and i[2] != '':
+                    event_list.add((key, i[0], i[1], i[2]))
 
     f = open('../data/%s_event_list.txt' % resource, 'w')
     for i in event_list:
         f.write(str(i) + '\n')
     f.close()
-
-    print('total of event: %d.' % c)
+    total += len(event_list)
+    print('total of event in %s: %d.' % (resource, len(event_list)))
+print('total of event: %d' % total)
 
---
+++ src/word2vec.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+# import modules & set up logging
+import gensim
+import logging
+import glob
+import os
+import string
+import re
+# from tqdm import tqdm
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+ 
+resources_dirs = ['../data/ReutersNews106521/*',
+        '../data/20061020_20131126_bloomberg_news/*']
+
+intab = string.punctuation \
+            + '～！@＃¥％……&＊（）｛｝［］｜、；：‘’“”，。／？《》＝＋－——｀' \
+            + '！‘’“”#￥%（）*+，-。、：；《》=？@【】·~——{|} '
+outtab = ' '*len(intab)
+transtab = string.maketrans(intab, outtab)
+
+class Mysentences(object):
+    """sentences iterator"""
+    def __init__(self, dirs):
+        self.dirs = dirs
+
+    def __iter__(self):
+        for dir in self.dirs:
+            for sub_dir in glob.glob(dir):
+                for txt in glob.glob(sub_dir + '/*'):
+                    f = open(txt, 'r')
+                    line = f.readline()
+                    while line:
+                        # line = re.subn(r'[\n\r\t]+', '', line)[0]        # 去除字符串中间的空格
+                        # line = re.subn(r'[0-9]+', '', line)[0]        # 去除字符串中间的空格
+                        # line = line.translate(transtab).strip()          # 去除字符串中的标点符号和位于字符串左边和右边的空格
+                        line = re.sub(r'[^a-z]+', ' ', line.lower()).strip()
+                        line = [word.strip() for word in line.split()]
+                        if len(line) > 0:
+                            yield line
+                        line = f.readline()
+                    f.close()
+
+
+if __name__ == '__main__':
+    if os.path.exists('../data/sg_model'):
+        model = gensim.models.Word2Vec.load('../data/sg_model')
+    else:
+        sentences = Mysentences(resources_dirs)
+        model = gensim.models.Word2Vec(sentences, size=100, min_count=5, sg=1)
+        model.save('../data/sg_model')
+
