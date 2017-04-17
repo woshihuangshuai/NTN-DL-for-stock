7fd39b07a8eb794a32782fa1081d5f60c9e2fafe
huangshuai <332987750@qq.com>
2017-04-17 14:37:39 +0800

    .

--- src/shell/batch_reverb.sh
+++ src/shell/batch_reverb.sh
@@ -8,6 +8,6 @@ fi
 for file in $filelist
 do
  filename=${file##*/}
- echo $save_dir$filename"_reverb"
-#  java -Xmx512m -jar ../../tools/reverb/reverb-latest.jar $save_dir$filename"_reverb"
+#  echo $save_dir$filename"_reverb"
+ java -Xmx512m -jar ../../tools/reverb/reverb-latest.jar $file|$save_dir$filename"_reverb"
 done
