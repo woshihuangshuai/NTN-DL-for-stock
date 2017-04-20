170474e1d34ed03b9dc5da1efbc2144a7a5bdfb4
huangshuai <332987750@qq.com>
2017-04-20 12:02:18 +0800

    update zpar bash

--- src/shell/batch_zpar_depparser.sh
+++ src/shell/batch_zpar_depparser.sh
@@ -1,16 +1,35 @@
-filelist=`find ../../data/zpar/pos -type f`
-save_dir="../../data/zpar/dep"
-
-if [ ! -d "../../data/zpar" ]; then
- mkdir ../../data/zpar
-fi
-if [ ! -d "../../data/zpar/dep" ]; then
- mkdir ../../data/zpar/dep
-fi
-
-for file in $filelist
+news_folders=(bloomberg reuters)
+for folder in ${news_folders[@]}
 do
- filename=${file##*/}
-#  echo $save_dir$filename"_zpar_dep"
- ./../../tools/zpar/dist/english.depparser/depparser $file $save_dir$filename"_zpar_dep" ../../tools/zpar/english-models/depparser
+ dirlist=`find ../../data/zpar/postagger/$folder/* -type d`
+
+ if [ ! -d "../../data/zpar/" ]; then
+  mkdir ../../data/zpar/
+ fi
+ if [ ! -d "../../data/zpar/depparser/" ]; then
+  mkdir ../../data/zpar/depparser/
+ fi
+ if [ ! -d "../../data/zpar/depparser/$folder/" ]; then
+  mkdir ../../data/zpar/depparser/$folder/
+ fi
+
+ for dir in $dirlist
+ do
+  dir_idx=${dir##*/}
+  save_dir="../../data/zpar/depparser/$folder/$dir_idx/"
+  echo $save_dir
+  if [ ! -d "../../data/zpar/depparser/$folder/$dir_idx/" ]; then
+   mkdir ../../data/zpar/depparser/$folder/$dir_idx/
+  fi
+
+  filelist=`find $dir/ -type f`
+
+  for file in $filelist
+  do
+   filename=${file##*/}
+#    echo $file
+#    echo $save_dir$filename"_zpar_pos"
+   ./../../tools/zpar/dist/english.depparser/depparser $file $save_dir$filename"_zpar_dep" ../../tools/zpar/english-models/depparser
+  done
+ done
 done
--- src/shell/batch_zpar_postagger.sh
+++ src/shell/batch_zpar_postagger.sh
@@ -1,16 +1,35 @@
-filelist=`find ../../data/processed_news/ -type f`
-save_dir="../../data/zpar/pos"
-
-if [ ! -d "../../data/zpar" ]; then
- mkdir ../../data/zpar
-fi
-if [ ! -d "../../data/zpar/pos" ]; then
- mkdir ../../data/zpar/pos
-fi
-
-for file in $filelist
+news_folders=(bloomberg reuters)
+for folder in ${news_folders[@]}
 do
- filename=${file##*/}
-#  echo $save_dir$filename"_zpar_pos"
- ./../../tools/zpar/dist/english.postagger/postagger $file $save_dir$filename"_zpar_pos" ../../tools/zpar/english-models/tagger
+ dirlist=`find ../../data/processed_news/$folder/* -type d`
+ 
+ if [ ! -d "../../data/zpar/" ]; then
+  mkdir ../../data/zpar/
+ fi
+ if [ ! -d "../../data/zpar/postagger/" ]; then
+  mkdir ../../data/zpar/postagger/
+ fi
+ if [ ! -d "../../data/zpar/postagger/$folder/" ]; then
+  mkdir ../../data/zpar/postagger/$folder/
+ fi
+
+ for dir in $dirlist
+ do
+  dir_idx=${dir##*/}
+  save_dir="../../data/zpar/postagger/$folder/$dir_idx/"
+  echo $save_dir
+  if [ ! -d "../../data/zpar/postagger/$folder/$dir_idx/" ]; then
+   mkdir ../../data/zpar/postagger/$folder/$dir_idx/
+  fi
+
+  filelist=`find ../../data/processed_news/$folder/$dir_idx/ -type f`
+
+  for file in $filelist
+  do
+   filename=${file##*/}
+#    echo $file
+#    echo $save_dir$filename"_zpar_pos"
+   ./../../tools/zpar/dist/english.postagger/tagger $file $save_dir$filename"_zpar_pos" ../../tools/zpar/english-models/tagger
+  done
+ done
 done
