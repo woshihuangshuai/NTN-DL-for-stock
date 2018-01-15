8b80a52613de42c7c06b3ba8cf7c247618fdba26
woshihuangshuai <jyaa2000133@gmail.com>
2018-01-15 16:18:40 +0800

    添加了一个脚本，将同一日期下的所有新闻标题和从新闻标题中提取的事件列出来，用于对比分析哪些句子可以被提取出事件，哪些不能，为什么不能

---
+++ src/extract/compare_extraction_result.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+folder_dir = '../../data/news_title/'
+news_title_file_name = 'reuters_news_title.txt'
+event_file_name = 'event/reuters_event.txt'
+compare_result_file_name = 'compare_result.txt'
+
+event_dic = {}
+with open(folder_dir + event_file_name, 'r') as event_file:
+    line = event_file.readline()
+    while line:
+        items = line.split('.')
+        if items[0] not in event_dic.keys():
+            event_dic[items[0]] = ['\t'.join(items[1:])]
+        else:
+            event_dic[items[0]].append('\t'.join(items[1:]))
+        line = event_file.readline()
+
+title_dic = {}
+with open(folder_dir + news_title_file_name, 'r') as news_title_file:
+    line = news_title_file.readline()
+    while line:
+        items = line.split('\t')
+        if items[0] not in title_dic.keys():
+            title_dic[items[0]] = [items[1]]
+        else:
+            title_dic[items[0]].append(items[1])
+        line = news_title_file.readline()
+
+split_line = '********************%s********************\n'
+news_split_line = '********************NEWS********************\n'
+event_split_line = '********************EVENT********************\n'
+with open(folder_dir + compare_result_file_name, 'w') as compare_result_file:
+    for key in title_dic.keys():
+        compare_result_file.write(split_line % key)
+        compare_result_file.write(news_split_line)
+        for news in title_dic[key]:
+            compare_result_file.write(news + '\n')
+        compare_result_file.write(event_split_line)
+        if key in event_dic.keys():
+            for event in event_dic[key]:
+                compare_result_file.write(event + '\n')
