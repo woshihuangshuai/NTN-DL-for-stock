4561b9b03e369b25d8607bf755570ca6e7ff655c
woshihuangshuai <jyaa2000133@gmail.com>
2018-01-16 16:50:31 +0800

    1. 更新了几个函数名，遵循Python命名规范 2. 修改了word2vec中对新闻数据的处理方法，现在使用merged_news作为输入数据，先用nltk对句子进行分词，然后去除句子中的特殊符号和数字，然后去除长度小于2的单词

--- src/extract/word2vec.py
+++ src/extract/word2vec.py
@@ -10,6 +10,7 @@ import string
 
 import gensim
 import numpy as np
+from nltk.tokenize import word_tokenize
 from tqdm import trange
 
 logging.basicConfig(
@@ -20,42 +21,41 @@ class Mysentences(object):
     """sentences iterator"""
 
     def __init__(self):
-        self.news_dir = '../../data/raw_news/'
-        self.news_folders = ['bloomberg', 'reuters']
+        self.news_dir = '../../data/merged_news/'
 
     def __iter__(self):
-        for folder in self.news_folders:
-            for sub_dir in glob.glob(self.news_dir + folder + '/*'):
-                for txt in glob.glob(sub_dir + '/*'):
-                    f = open(txt, 'r')
+        for news_dir in glob.glob(self.news_dir + '/*'):
+            with open(news_dir, 'r') as f:
+                line = f.readline()
+                while line:
+                    words = [re.sub(r'[^a-z]+', ' ', word).strip()
+                             for word in word_tokenize(line)]  # 去除 除了小写字符a到z之外的所有字符，包括特殊符号和数字
+                    words = [word for word in words if len(
+                        word) > 1]  # 去除长度小于2的单词
+                    if len(words) > 0:
+                        yield words
                     line = f.readline()
-                    while line:
-                        line = re.sub(r'[^a-z]+', ' ', line.lower()).strip()
-                        line = [word.strip() for word in line.split()]
-                        if len(line) > 0:
-                            yield line
-                        line = f.readline()
-                    f.close()
 
 
-def getModel():
+def get_word2vec_model():
+    '''从文件中导入或训练生成Word2Vec模型'''
     model = None
     if os.path.exists('../../data/sg_model/sg_model'):
         model = gensim.models.Word2Vec.load('../../data/sg_model/sg_model')
     else:
         sentences = Mysentences()
-        model = gensim.models.Word2Vec(sentences, size=100, min_count=1, sg=1)
+        model = gensim.models.Word2Vec(sentences, size=100, min_count=5, sg=1)
         model.save('../../data/sg_model')
     return model
 
 
-def event2Vec_NewsTitle():
+def get_event_embedding_from_news_title_event():
     print 'Transforming news title event to word_embedding.'
-    dir_path = '../../data/news_title/event/*'
+    source_dir = '../../data/news_title/event/*'
     save_dir = '../../data/event_embedding/news_title/'
 
-    event_file_list = [file for file in glob.glob(dir_path)]
-    model = getModel()
+    event_file_list = [file for file in glob.glob(source_dir)]
+    model = get_word2vec_model()
 
     if model != None:
         event2VecNewsTitle(model, event_file_list, save_dir)
@@ -85,7 +85,6 @@ def event2VecNewsTitle(model, event_file_list, save_dir):
                             # word-embeddings of one day's events 
                         ]
     '''
-
     event_embedding_dic = {}
     for file in event_file_list:
         print 'Transforming %s event into word-embedding.' % file
@@ -93,21 +92,28 @@ def event2VecNewsTitle(model, event_file_list, save_dir):
             line = event_file.readline()
             while line:
                 event_embedding = []
-                t = line.strip().split(',')
-                datetime = t[0]
-                del t[0]
-                if len(t) != 3:
+                args = line.strip().split(',')
+                if len(args) != 4:
                     line = event_file.readline()
                     continue
-                for arg in t:
-                    word_list = arg.split()
-                    length = len(word_list)
+                datetime = args[0]
+
+                for arg in args[1:]:
+                    # 去除 除了小写字符a到z之外的所有字符，包括特殊符号和数字
+                    words = [re.sub(r'[^a-z]+', ' ', word).strip()
+                             for word in word_tokenize(arg)]
+                    # 去除长度小于2的单词
+                    words = [word for word in words if len(
+                        word) > 1]
+
+                    length = len(words)
                     sum = np.zeros(100)
-                    for word in word_list:
+                    for word in words:
                         try:
                             sum += model.wv[word]
                         except:
-                            line = event_file.readline()
+                            length -= 1
+                            # TODO 不能向量化的单词用随机初始化的向量表示
                             continue
                     mean = sum / length
                     event_embedding.append(mean)
@@ -116,42 +122,43 @@ def event2VecNewsTitle(model, event_file_list, save_dir):
                     event_embedding_dic[datetime] = [event_embedding]
                 else:
                     event_embedding_dic[datetime].append(event_embedding)
+
                 line = event_file.readline()
 
-    # Normalization
-    all_word_embeddings = []
-    for key in event_embedding_dic.keys():
-        all_word_embeddings.extend(
-            [word_embedding for word_embedding in event_embedding_dic[key]])
+    # # Normalization
+    # all_word_embeddings = []
+    # for key in event_embedding_dic.keys():
+    #     all_word_embeddings.extend(
+    #         [word_embedding for word_embedding in event_embedding_dic[key]])
 
-    all_word_embeddings_array = np.array(all_word_embeddings)
-    min_word_embedding = all_word_embeddings_array.min(axis=0)
-    max_word_embedding = all_word_embeddings_array.max(axis=0)
-    max_minus_min = max_word_embedding - min_word_embedding
+    # all_word_embeddings_array = np.array(all_word_embeddings)
+    # min_word_embedding = all_word_embeddings_array.min(axis=0)
+    # max_word_embedding = all_word_embeddings_array.max(axis=0)
+    # max_minus_min = max_word_embedding - min_word_embedding
 
-    for key in event_embedding_dic.keys():
-        for i in range(len(event_embedding_dic[key])):
-            t = event_embedding_dic[key][i]
-            t = (t - min_word_embedding) / max_minus_min
-            event_embedding_dic[key][i] = t
+    # for key in event_embedding_dic.keys():
+    #     for i in range(len(event_embedding_dic[key])):
+    #         t = event_embedding_dic[key][i]
+    #         t = (t - min_word_embedding) / max_minus_min
+    #         event_embedding_dic[key][i] = t
 
     # persistence
-    print 'persisting...'
+    print 'Persisting...'
+    if os.path.exists(save_dir) == False:
+        os.makedirs(save_dir)
     for datetime in event_embedding_dic.keys():
-        if os.path.exists(save_dir) == False:
-            os.makedirs(save_dir)
         npzfile = datetime + '.npy'
         np.save(save_dir + npzfile, event_embedding_dic[datetime])
 
 
-def event2Vec_AllNews():
+def get_event_embedding_from_news_content_event():
     print 'Transforming all news event to event_embedding.'
     news_dir = '../../data/event/*'
     save_dir = '../../data/event_embedding/all_news/'
 
     event_file_list = [file for file in glob.glob(
         news_dir) if len(file.split('/')[-1]) == 8]
-    model = getModel()
+    model = get_word2vec_model()
 
     if model != None:
         event2VecAllNews(model, event_file_list, save_dir)
@@ -181,64 +188,73 @@ def event2VecAllNews(model, event_file_list, save_dir):
                             # event-embeddings of one day 
                         ]
     '''
-    if os.path.exists(save_dir) == False:
-        os.makedirs(save_dir)
-
     event_embedding_dic = {}
     for file_idx in trange(len(event_file_list), desc='Main progress'):
         file = event_file_list[file_idx]
         filename = file.split('/')[-1]
         event_embedding_list = []
-
+        
         with open(file, 'r') as event_file:
             lines = event_file.readlines()
             for line_idx in trange(len(lines), desc='%s' % filename):
                 line = lines[line_idx]
                 event_embedding = []
-                t = line.strip().split(',')
-                if len(t) != 3:
+
+                args = line.strip().split(',')
+                if len(args) != 3:
                     continue
-                for arg in t:
-                    word_list = arg.split()
-                    length = len(word_list)
+
+                for arg in args:
+                    # 去除 除了小写字符a到z之外的所有字符，包括特殊符号和数字
+                    words = [re.sub(r'[^a-z]+', ' ', word).strip()
+                             for word in word_tokenize(arg)]
+                    # 去除长度小于2的单词
+                    words = [word for word in words if len(
+                        word) > 1]
+
+                    length = len(words)
                     sum = np.zeros(100)
-                    for word in word_list:
+                    for word in words:
                         try:
                             sum += model.wv[word]
                         except:
+                            length -= 1
+                            # TODO 不能向量化的单词用随机初始化的向量表示
                             continue
                     mean = sum / length
                     event_embedding.append(mean)
+
                 event_embedding_list.append(event_embedding)
                 line = event_file.readline()
+                
         event_embedding_dic[filename] = event_embedding_list
 
-    # Normalization
-    all_word_embeddings = []
-    for key in event_embedding_dic.keys():
-        all_word_embeddings.extend(
-            [word_embedding for word_embedding in event_embedding_dic[key]])
+    # # Normalization
+    # all_word_embeddings = []
+    # for key in event_embedding_dic.keys():
+    #     all_word_embeddings.extend(
+    #         [word_embedding for word_embedding in event_embedding_dic[key]])
 
-    all_word_embeddings_array = np.array(all_word_embeddings)
-    min_word_embedding = all_word_embeddings_array.min(axis=0)
-    max_word_embedding = all_word_embeddings_array.max(axis=0)
-    max_minus_min = max_word_embedding - min_word_embedding
+    # all_word_embeddings_array = np.array(all_word_embeddings)
+    # min_word_embedding = all_word_embeddings_array.min(axis=0)
+    # max_word_embedding = all_word_embeddings_array.max(axis=0)
+    # max_minus_min = max_word_embedding - min_word_embedding
 
-    for key in event_embedding_dic.keys():
-        for i in range(len(event_embedding_dic[key])):
-            t = event_embedding_dic[key][i]
-            t = (t - min_word_embedding) / max_minus_min
-            event_embedding_dic[key][i] = t
+    # for key in event_embedding_dic.keys():
+    #     for i in range(len(event_embedding_dic[key])):
+    #         t = event_embedding_dic[key][i]
+    #         t = (t - min_word_embedding) / max_minus_min
+    #         event_embedding_dic[key][i] = t
 
     # persistence
     print 'persisting...'
+    if os.path.exists(save_dir) == False:
+        os.makedirs(save_dir)
     for datetime in event_embedding_dic.keys():
-        if os.path.exists(save_dir) == False:
-            os.makedirs(save_dir)
         npzfile = datetime + '.npy'
         np.save(save_dir + npzfile, event_embedding_dic[datetime])
 
 
 if __name__ == '__main__':
-    event2Vec_NewsTitle()
-    event2Vec_AllNews()
+    get_event_embedding_from_news_title_event()
+    get_event_embedding_from_news_content_event()
