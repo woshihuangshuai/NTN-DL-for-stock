407a7e029117bc2bb1f4a1f9386aefcb01c2d2e8
woshihuangshuai <jyaa2000133@gmail.com>
2018-03-30 17:07:46 +0800

    分别对merged_raw_news和merged_processed_news目录下的新闻文件进行合并

--- src/shell/merge_file.sh
+++ src/shell/merge_file.sh
@@ -1,8 +1,27 @@
 # 将一个日期目录下的所有新闻，合并为同一个文件，以加速ReVerb和ZPar的速度
 dirlist=`find ../../data/processed_news/* -type d`
-save_dir="../../data/merged_news/"
+save_dir="../../data/merged_processed_news/"
 
 if [ ! -d "../../data/merged_news/" ]; then
+ mkdir ../../data/merged_news/
+fi
+
+for dir in $dirlist
+do
+ filename=${dir##*/}
+ echo $dir
+ filelist=`find $dir/ -type f`
+ for file in $filelist
+ do
+  cat $file >> $save_dir$filename
+ done
+done
+
+
+dirlist=`find ../../data/raw_news/* -type d`
+save_dir="../../data/merged_raw_news/"
+
+if [ ! -d "../../data/merged_raw_news/" ]; then
  mkdir ../../data/merged_news/
 fi
 
