5413384e1ffa72522cba4b784babbc23893adc2e
woshihuangshuai <jyaa2000133@gmail.com>
2018-03-30 17:08:24 +0800

    使用合并后的原始文本数据作为ReVerb的输入

--- src/shell/batch_reverb.sh
+++ src/shell/batch_reverb.sh
@@ -1,11 +1,11 @@
-# 批量对合并后的新闻文件进行ReVerb处理，以句子为单位提取句子中的三元候选元组
+# 批量对合并后的原始新闻文件进行ReVerb处理，以句子为单位提取句子中的三元候选元组，实验证明使用原始新闻提取的结果更好
 save_dir="../../data/reverb/"
 
 if [ ! -d "../../data/reverb/" ]; then
  mkdir ../../data/reverb/
 fi
 
-filelist=`find ../../data/merged_news/ -type f`
+filelist=`find ../../data/merged_raw_news/ -type f`
 for file in $filelist
 do
  filename=${file##*/}
