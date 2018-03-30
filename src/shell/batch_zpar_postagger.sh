c1f2cf9ed7950cdcc3f282107195442fe3f4ca2e
woshihuangshuai <jyaa2000133@gmail.com>
2018-03-30 17:09:28 +0800

    使用合并后的处理过的文本数据作为ZPar的输入

--- src/shell/batch_zpar_postagger.sh
+++ src/shell/batch_zpar_postagger.sh
@@ -1,5 +1,6 @@
 # ZPar处理新闻分为两步，此为第一步<词性标注>
 # 对合并后的新闻文件进行ZPar的词性标注操作
+
 save_dir="../../data/zpar/postagger/"
 
 if [ ! -d "../../data/zpar/" ]; then
@@ -9,7 +10,7 @@ if [ ! -d "../../data/zpar/postagger/" ]; then
  mkdir ../../data/zpar/postagger/
 fi
 
-filelist=`find ../../data/merged_news/ -type f`
+filelist=`find ../../data/merged_processed_news/ -type f`
 for file in $filelist
 do
  filename=${file##*/}
